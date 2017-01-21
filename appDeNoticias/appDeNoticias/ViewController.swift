626b06ad91a4ee89434542608bbf031ca76c9076
Helio Mesquita <mesquitahelio@hotmail.com>
2017-01-21 15:29:32 -0200

    primeiro commit do app - estrutura básica do xcode Swift

---
+++ appDeNoticias/appDeNoticias/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appDeNoticias
+//
+//  Created by Sábado on 21/01/17.
+//  Copyright © 2017 Sábado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
